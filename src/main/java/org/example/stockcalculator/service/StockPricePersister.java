7df8da7970df0ad1e03d338fba9d172f8202a0f6
Adelin Pachedzhiev <adelin.pachedzhiev@sap.com>
2025-04-27 11:43:40 +0300

    optimize stock price retrieval

--- src/main/java/org/example/stockcalculator/service/StockPricePersister.java
+++ src/main/java/org/example/stockcalculator/service/StockPricePersister.java
@@ -1,18 +1,19 @@
 package org.example.stockcalculator.service;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.example.stockcalculator.util.SleepUtil.sleepSilentlyForSeconds;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 
 import org.example.stockcalculator.entity.Stock;
 import org.example.stockcalculator.entity.StockPriceEntity;
 import org.example.stockcalculator.model.StockPrice;
 import org.example.stockcalculator.repository.StockPriceRepository;
 import org.example.stockcalculator.repository.StockRepository;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,24 +22,42 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class StockPricePersister {
 
+    private static final int API_REQUESTS_PER_MINUTE_LIMIT = 60;
     private final StockPriceApiClient stockPriceApiClient;
     private final StockPriceRepository stockPriceRepository;
     private final StockRepository stockRepository;
+    private final ExecutorService executorService;
 
-    @Scheduled(fixedRate = 8, timeUnit = SECONDS)
-    public void persistStockPrice() {
+    @PostConstruct
+    public void startPersistingStockPrices() {
+        executorService.submit(this::persistStockPricesForever);
+    }
+
+    private void persistStockPricesForever() {
+        while (!Thread.currentThread().isInterrupted()) {
+            getStocksAndPersistTheirPrices();
+        }
+    }
+
+    private void getStocksAndPersistTheirPrices() {
         List<Stock> supportedStocks = stockRepository.findAll();
-//        log.info("Supported stocks: {}", supportedStocks.stream().map(Stock::getSymbol).toList());
-        supportedStocks.forEach(this::getPriceAndSaveToDb);
+        int delayBetweenRequests = calculateDelayBetweenRequests(supportedStocks);
+        supportedStocks.forEach((stock -> {
+            sleepSilentlyForSeconds(delayBetweenRequests);
+            getPriceAndSaveToDb(stock);
+        }));
+    }
+
+    private int calculateDelayBetweenRequests(List<Stock> supportedStocks) {
+        int stocksCount = supportedStocks.size();
+        int requestsForAStockPerMinute = API_REQUESTS_PER_MINUTE_LIMIT / stocksCount;
+        return 60 / requestsForAStockPerMinute;
     }
 
     private void getPriceAndSaveToDb(Stock stock) {
         stockPriceApiClient.getPriceForSymbol(stock.getSymbol())
                 .map(price -> createStockPriceEntity(stock, price))
-                .ifPresent(priceEntity -> {
-                    stockPriceRepository.save(priceEntity);
-//                    log.info("Saved stock price {} for stock {}", priceEntity.getPrice(), stock.getName());
-                });
+                .ifPresent(stockPriceRepository::save);
     }
 
     private StockPriceEntity createStockPriceEntity(Stock stock, StockPrice price) {
