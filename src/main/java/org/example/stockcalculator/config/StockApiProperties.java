1d5b8c0a95d7737b8188518a6218e28af9952fe8
Adelin Pachedzhiev <56289229+Adelin-Pachedzhiev@users.noreply.github.com>
2025-05-12 07:50:20 +0300

    change app properties

--- src/main/java/org/example/stockcalculator/config/StockApiProperties.java
+++ src/main/java/org/example/stockcalculator/config/StockApiProperties.java
@@ -2,6 +2,6 @@ package org.example.stockcalculator.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "stock.api")
+@ConfigurationProperties(prefix = "stock.prices-api")
 public record StockApiProperties(String url, String apiKey) {
 }
---
+++ src/main/java/org/example/stockcalculator/config/StockTransactionApiProperties.java
@@ -0,0 +1,9 @@
+package org.example.stockcalculator.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "stock.transactions-api")
+public record StockTransactionApiProperties(
+        String url,
+        String apiKey
+) {}
