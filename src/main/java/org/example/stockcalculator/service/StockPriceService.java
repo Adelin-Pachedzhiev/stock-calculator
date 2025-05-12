b2ff6cceb4ef08f726cbdac45af520c2c57ddd0e
Adelin Pachedzhiev <56289229+Adelin-Pachedzhiev@users.noreply.github.com>
2025-05-12 07:49:35 +0300

    add service for getting current price

---
+++ src/main/java/org/example/stockcalculator/service/StockPriceService.java
@@ -0,0 +1,28 @@
+package org.example.stockcalculator.service;
+
+import java.util.Optional;
+
+import org.example.stockcalculator.entity.StockPriceEntity;
+import org.example.stockcalculator.model.StockPrice;
+import org.example.stockcalculator.repository.StockPriceRepository;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class StockPriceService {
+
+    private final StockPriceRepository stockPriceRepository;
+
+    public Optional<StockPrice> getCurrentPrice(String stockSymbol) {
+        StockPriceEntity currentPriceEntity = stockPriceRepository.findTopByStockSymbolOrderByTimestampDesc(stockSymbol);
+        if (currentPriceEntity == null) {
+            return Optional.empty();
+        }
+        StockPrice stockPrice = new StockPrice(stockSymbol, currentPriceEntity.getPrice());
+
+        return Optional.of(stockPrice);
+    }
+
+}
