5d9853bc0604925a0038e6424fb00bf129d2f4bc
Adelin Pachedzhiev <56289229+Adelin-Pachedzhiev@users.noreply.github.com>
2025-06-21 11:50:25 +0300

    add trading 212 secret management controller

---
+++ src/main/java/org/example/stockcalculator/integration/trading212/Trading212IntegrationController.java
@@ -0,0 +1,39 @@
+package org.example.stockcalculator.integration.trading212;
+
+import static org.example.stockcalculator.auth.utils.AuthUtils.currentUserId;
+import static org.example.stockcalculator.entity.IntegrationPlatform.TRADING212;
+
+import java.util.Map;
+
+import org.example.stockcalculator.entity.UserAccount;
+import org.example.stockcalculator.entity.UserIntegrationSecret;
+import org.example.stockcalculator.repository.IntegrationSecretRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/integration/trading212")
+@RequiredArgsConstructor
+public class Trading212IntegrationController {
+
+    private final IntegrationSecretRepository integrationSecretRepository;
+
+    @PostMapping
+    public ResponseEntity<?> addIntegration(Map<String, String > requestBody) {
+        String secretTxt = requestBody.get("secret");
+
+        UserIntegrationSecret integrationSecret = new UserIntegrationSecret();
+        integrationSecret.setUserAccount(new UserAccount(currentUserId()));
+        integrationSecret.setSecret(secretTxt);
+        integrationSecret.setPlatform(TRADING212);
+
+        integrationSecretRepository.save(integrationSecret);
+
+        return ResponseEntity.ok(Map.of("message", "Trading212 integration added successfully"));
+    }
+
+}
