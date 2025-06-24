2e7157e9f83da2d4081173b6631bb0ce4c4889f6
Adelin Pachedzhiev <56289229+Adelin-Pachedzhiev@users.noreply.github.com>
2025-06-24 20:29:39 +0300

    refactor: rename transaction request class and add update/delete methods

--- src/main/java/org/example/stockcalculator/service/StockTransactionService.java
+++ src/main/java/org/example/stockcalculator/service/StockTransactionService.java
@@ -2,7 +2,7 @@ package org.example.stockcalculator.service;
 
 import static org.example.stockcalculator.auth.utils.AuthUtils.currentUserId;
 
-import org.example.stockcalculator.dto.CreateTransactionRequest;
+import org.example.stockcalculator.dto.TransactionPayload;
 import org.example.stockcalculator.entity.StockTransaction;
 import org.example.stockcalculator.entity.UserAccount;
 import org.example.stockcalculator.mapper.StockTransactionMapper;
@@ -18,10 +18,22 @@ public class StockTransactionService {
     private final StockTransactionRepository stockTransactionRepository;
     private final StockTransactionMapper stockTransactionMapper;
 
-    public void saveStockTransaction(CreateTransactionRequest stockTransaction) {
+    public void saveStockTransaction(TransactionPayload stockTransaction) {
         StockTransaction stockTransactionEntity = stockTransactionMapper.toEntity(stockTransaction);
         stockTransactionEntity.setUser(new UserAccount(currentUserId()));
         stockTransactionRepository.save(stockTransactionEntity);
+    }
+
+    public void updateStockTransaction(Long transactionId, TransactionPayload stockTransaction) {
+        StockTransaction stockTransactionEntity = stockTransactionMapper.toEntity(stockTransaction);
+        stockTransactionEntity.setUser(new UserAccount(currentUserId()));
+        stockTransactionEntity.setId(transactionId);
+
+        stockTransactionRepository.save(stockTransactionEntity);
+    }
+
+    public void deleteStockTransaction(Long transactionId) {
+        stockTransactionRepository.deleteById(transactionId);
     }
 
 }
