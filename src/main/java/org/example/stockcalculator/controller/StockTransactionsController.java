bfca27b9ba7b72292918240acec9ebcafcef8ff1
Adelin Pachedzhiev <56289229+Adelin-Pachedzhiev@users.noreply.github.com>
2025-06-24 20:30:08 +0300

    feat: add update and delete methods for stock transactions

--- src/main/java/org/example/stockcalculator/controller/StockTransactionsController.java
+++ src/main/java/org/example/stockcalculator/controller/StockTransactionsController.java
@@ -4,13 +4,16 @@ import static org.example.stockcalculator.auth.utils.AuthUtils.currentUserId;
 
 import java.util.List;
 
-import org.example.stockcalculator.dto.CreateTransactionRequest;
+import org.example.stockcalculator.dto.TransactionPayload;
 import org.example.stockcalculator.entity.StockTransaction;
 import org.example.stockcalculator.integration.StockTransactionManager;
 import org.example.stockcalculator.repository.StockTransactionRepository;
 import org.example.stockcalculator.service.StockTransactionService;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +31,7 @@ public class StockTransactionsController {
     private final StockTransactionManager manager;
 
     @PostMapping
-    public void createTransaction(@RequestBody @Valid CreateTransactionRequest request) {
+    public void createTransaction(@RequestBody @Valid TransactionPayload request) {
         stockTransactionService.saveStockTransaction(request);
     }
 
@@ -38,6 +41,17 @@ public class StockTransactionsController {
 
         return stockTransactionRepository.findByUserIdOrderByTimeOfTransactionAsc(userId);
     }
+
+    @PutMapping("/{transactionId}")
+    public void updateTransaction(@RequestBody @Valid TransactionPayload request, @PathVariable Long transactionId) {
+        stockTransactionService.updateStockTransaction(transactionId, request);
+    }
+
+    @DeleteMapping("/{transactionId}")
+    public void deleteTransaction(@PathVariable Long transactionId) {
+        stockTransactionService.deleteStockTransaction(transactionId);
+    }
+
 
 
     @PostMapping("sync")
