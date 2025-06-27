95bd5fd15c2d77b46dcce717e37d5279495bf3ec
Adelin Pachedzhiev <56289229+Adelin-Pachedzhiev@users.noreply.github.com>
2025-06-27 08:21:58 +0300

    feat: enable prices-api and remove api-key from StockTransactionApiProperties

--- src/main/java/org/example/stockcalculator/config/StockTransactionApiProperties.java
+++ src/main/java/org/example/stockcalculator/config/StockTransactionApiProperties.java
@@ -4,6 +4,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "stock.transactions-api")
 public record StockTransactionApiProperties(
-        String url,
-        String apiKey
+        String url
 ) {}
