145eac78b4cba63e60ed78da0cb12136efeba284
Adelin Pachedzhiev <56289229+Adelin-Pachedzhiev@users.noreply.github.com>
2025-06-27 08:20:25 +0300

    feat: implement transaction synchronization and metadata handling in StockTransactionManager

--- src/main/java/org/example/stockcalculator/integration/StockTransactionManager.java
+++ src/main/java/org/example/stockcalculator/integration/StockTransactionManager.java
@@ -1,43 +1,115 @@
 package org.example.stockcalculator.integration;
 
+import static org.example.stockcalculator.auth.utils.AuthUtils.currentUserId;
 import static org.example.stockcalculator.entity.TransactionType.BUY;
 import static org.example.stockcalculator.entity.TransactionType.SELL;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.example.stockcalculator.entity.Stock;
 import org.example.stockcalculator.entity.StockTransaction;
 import org.example.stockcalculator.entity.UserAccount;
-import org.example.stockcalculator.integration.trading212.Trading212StockTransactionsClient;
+import org.example.stockcalculator.entity.UserIntegrationSecret;
+import org.example.stockcalculator.integration.trading212.Trading212ApiClient;
+import org.example.stockcalculator.integration.trading212.dto.Trading212InstrumentMetadata;
+import org.example.stockcalculator.integration.trading212.dto.Trading212Transaction;
+import org.example.stockcalculator.integration.trading212.dto.Trading212UserInfo;
+import org.example.stockcalculator.integration.trading212.dto.TransactionTax;
+import org.example.stockcalculator.repository.IntegrationSecretRepository;
+import org.example.stockcalculator.repository.StockRepository;
 import org.example.stockcalculator.repository.StockTransactionRepository;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class StockTransactionManager {
 
-    private final Trading212StockTransactionsClient stockTransactionsClient;
+    public static final double EUR_USD_RATE = 1.17;
+    private final Trading212ApiClient apiClient;
     private final StockTransactionRepository stockTransactionRepository;
+    private final IntegrationSecretRepository integrationSecretRepository;
+    private final StockRepository stockRepository;
 
-    public void syncTransactionsToDb() {
-        List<StockTransaction> stockTransactions = stockTransactionsClient.fetchTransactionsOfUser()
+    public void syncTransactionsToDbForIntegration(Long integrationId) {
+        Optional<UserIntegrationSecret> integrationOpt = integrationSecretRepository.findById(integrationId);
+        if (integrationOpt.isEmpty()) {
+            throw new IllegalArgumentException("Integration with ID " + integrationId + " not found.");
+        }
+        UserIntegrationSecret integration = integrationOpt.get();
+        String secret = integration.getSecret();
+
+        Trading212UserInfo userInfo = apiClient.getUserInfo(secret);
+        String mainCurrencyForAccount = userInfo.currencyCode();
+
+        List<Trading212InstrumentMetadata> instrumentMetadata = apiClient.fetchInstrumentsMetadataForIntegration(secret);
+        List<Stock> stocks = stockRepository.findAll();
+
+        List<StockTransaction> stockTransactions = apiClient.fetchTransactionsForIntegration(secret)
                 .stream()
-                .map(StockTransactionManager::convertToStockTransactionEntity)
+                .flatMap(tx -> convertToStockTransactionEntity(tx, instrumentMetadata, stocks, mainCurrencyForAccount).stream())
                 .toList();
+
+        log.info("Saving {} stock transactions for integration ID {}", stockTransactions.size(), integrationId);
         stockTransactionRepository.saveAll(stockTransactions);
     }
 
-    private static StockTransaction convertToStockTransactionEntity(Trading212StockTransactionsClient.Transaction tx) {
+    private Optional<StockTransaction> convertToStockTransactionEntity(Trading212Transaction tx,
+            List<Trading212InstrumentMetadata> instrumentMetadata,
+            List<Stock> stocks,
+            String mainCurrencyForAccount) {
+
+        Trading212InstrumentMetadata stockMetadata = instrumentMetadata.stream()
+                .filter(m -> m.ticker().equals(tx.ticker()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Ticker " + tx.ticker() + " not found in metadata."));
+
+        Optional<Stock> stock = stocks.stream()
+                .filter(s -> s.getSymbol().equals(stockMetadata.shortName()))
+                .findFirst();
+        if (stock.isEmpty()) {
+            return Optional.empty();
+        }
+
         StockTransaction stockTransaction = new StockTransaction();
-        stockTransaction.setStock(new Stock(1L));
-        stockTransaction.setUser(new UserAccount(1L));
+        stockTransaction.setStock(stock.get());
+        stockTransaction.setUser(new UserAccount(currentUserId()));
         stockTransaction.setPrice(tx.fillPrice());
-        stockTransaction.setQuantity(tx.orderedValue() / tx.fillPrice());
+        stockTransaction.setFee(calculateFee(tx, mainCurrencyForAccount));
         stockTransaction.setTimeOfTransaction(tx.dateModified());
-        stockTransaction.setFee(0.0); // todo add fee
-        stockTransaction.setType(tx.orderedValue() > 0 ? BUY : SELL);
-        return stockTransaction;
+        stockTransaction.setCurrency(stockMetadata.currencyCode());
+
+        computeQuantityAndType(tx, mainCurrencyForAccount, stockMetadata, stockTransaction);
+
+        return Optional.of(stockTransaction);
+    }
+
+    private static void computeQuantityAndType(Trading212Transaction tx, String mainCurrencyForAccount, Trading212InstrumentMetadata stockMetadata,
+            StockTransaction stockTransaction) {
+        if (tx.orderedQuantity() == null) {
+            double orderedValue = tx.orderedValue();
+            if (!stockMetadata.currencyCode().equals(mainCurrencyForAccount)) {
+                orderedValue *= EUR_USD_RATE;
+            }
+            stockTransaction.setQuantity(orderedValue  / tx.fillPrice());
+            stockTransaction.setType(tx.orderedValue() > 0 ? BUY : SELL);
+        }
+        else {
+            stockTransaction.setQuantity(tx.orderedQuantity());
+            stockTransaction.setType(tx.orderedQuantity() > 0 ? BUY : SELL);
+            throw new UnsupportedOperationException("Ordered quantity is not supported yet.");
+        }
+    }
+
+    private double calculateFee(Trading212Transaction tx, String mainCurrencyForAccount) {
+        double taxAmount = -tx.taxes().stream().mapToDouble(TransactionTax::quantity).sum();
+        if (mainCurrencyForAccount.equals("EUR")) {
+            taxAmount*= EUR_USD_RATE;
+        }
+        return taxAmount;
     }
 }
