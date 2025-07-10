c10373b3dcdbd1fcb73c3ed3e417b5032b3dbfbb
Adelin Pachedzhiev <56289229+Adelin-Pachedzhiev@users.noreply.github.com>
2025-07-10 14:13:45 +0300

    feat: add methods to StockRepository for finding stock by symbol and retrieving all symbols

--- src/main/java/org/example/stockcalculator/repository/StockRepository.java
+++ src/main/java/org/example/stockcalculator/repository/StockRepository.java
@@ -1,8 +1,16 @@
 package org.example.stockcalculator.repository;
 
+import java.util.Optional;
+import java.util.Set;
+
 import org.example.stockcalculator.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
+    Optional<Stock> findBySymbol(String symbol);
+
+    @Query("SELECT s.symbol FROM Stock s")
+    Set<String> findAllSymbols();
 }
