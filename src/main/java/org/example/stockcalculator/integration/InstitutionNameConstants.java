a33fd9d8fa493a17438e7d35139d5bf6e9d84b8a
Adelin Pachedzhiev <56289229+Adelin-Pachedzhiev@users.noreply.github.com>
2025-07-14 08:23:40 +0300

    feat: add InstitutionNameConstants for integration with trading platforms

---
+++ src/main/java/org/example/stockcalculator/integration/InstitutionNameConstants.java
@@ -0,0 +1,7 @@
+package org.example.stockcalculator.integration;
+
+public class InstitutionNameConstants {
+    public static final String TRADING212 = "Trading212";
+    public static final String INTERACTIVE_BROKERS = "Interactive Brokers";
+
+}
