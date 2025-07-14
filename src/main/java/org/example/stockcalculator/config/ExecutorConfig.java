41a484ced56ecd21139c6817964a174f1b0e72b2
Adelin Pachedzhiev <56289229+Adelin-Pachedzhiev@users.noreply.github.com>
2025-07-14 08:19:53 +0300

    fix: reorder import statements in ExecutorConfig for consistency

--- src/main/java/org/example/stockcalculator/config/ExecutorConfig.java
+++ src/main/java/org/example/stockcalculator/config/ExecutorConfig.java
@@ -1,10 +1,10 @@
 package org.example.stockcalculator.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ExecutorConfig {
