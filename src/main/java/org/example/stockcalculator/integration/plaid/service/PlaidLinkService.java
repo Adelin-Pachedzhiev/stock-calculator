a6cc90de86ac2970e0a154a3d2f4eaf5a77b839d
Adelin Pachedzhiev <56289229+Adelin-Pachedzhiev@users.noreply.github.com>
2025-07-16 09:04:45 +0300

    refactor: add additional product consent options in PlaidLinkService

--- src/main/java/org/example/stockcalculator/integration/plaid/service/PlaidLinkService.java
+++ src/main/java/org/example/stockcalculator/integration/plaid/service/PlaidLinkService.java
@@ -44,6 +44,9 @@ public class PlaidLinkService {
                 .clientName("Portfolio Tracker")
                 .user(user)
                 .products(List.of(Products.INVESTMENTS))
+                .addAdditionalConsentedProductsItem(Products.INVESTMENTS)
+                .addOptionalProductsItem(Products.INVESTMENTS)
+                .addRequiredIfSupportedProductsItem(Products.INVESTMENTS)
                 .language("en")
                 .countryCodes(List.of(US, CA));
 
